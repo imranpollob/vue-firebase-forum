d3cc23f0aae4a608cd5809acde075accebaeaddc
Imran <polboy777@gmail.com>
2019-10-17 13:42:56 +0600

    udpate forum style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>Welcome to the forum</h1>
+  <div>
+    <div v-for="thread in threads" class="col-large push-top" :key="thread['.key']">
+      <h1>{{thread.title}}</h1>
 
-    <div v-for="thread in threads" :key="thread['.key']">
-      <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{ users[posts[postId].userId].name }}</p>
-        <p>{{ posts[postId].text }}</p>
+      <div class="post-list">
+        <div v-for="postId in thread.posts" class="post" :key="postId">
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt />
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>{{posts[postId].text}}</div>
+          </div>
+
+          <div class="post-date text-faded">{{posts[postId].publishedAt}}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -14,6 +28,7 @@
 
 <script>
 import sourceData from "@/data";
+console.log(sourceData);
 
 export default {
   name: "HelloWorld",
