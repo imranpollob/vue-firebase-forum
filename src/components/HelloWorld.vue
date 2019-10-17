fe2124b15292061b43a7475eefe96a84a03214dc
Imran <polboy777@gmail.com>
2019-10-17 13:18:14 +0600

    showing the threads with post and author name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Pollob</h2>
+    <h1>Welcome to the forum</h1>
+
+    <div v-for="thread in threads" :key="thread['.key']">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import sourceData from "./../../src/data";
+
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    };
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
