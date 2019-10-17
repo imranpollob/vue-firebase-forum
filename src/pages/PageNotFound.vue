7b67037566c0153d084ffcd5e2fa155338e45d3f
Imran <polboy777@gmail.com>
2019-10-17 14:52:35 +0600

    modul style in PageNotFound page

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="col-full">
-    <h1>Not Found</h1>
-    <p>
+  <div class="col-full" :class="$style.centered">
+    <h1 :class="$style.headingLarge">Not Found</h1>
+    <p class="text-large" :class="$style.textLarge">
       Ooops, we couldn't find what you are looking for. Why don't you
       <router-link :to="{name: 'Home'}">go home instead?</router-link>
     </p>
@@ -12,15 +12,15 @@
 export default {};
 </script>
 
-<style scoped>
-h1 {
+<style module>
+.headingLarge {
   font-size: 100px;
 }
-p {
+.textLarge {
   font-size: 50px;
   font-weight: 100;
 }
-div {
+.centered {
   text-align: center;
 }
 </style>
