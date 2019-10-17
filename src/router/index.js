0ace3d578e4761279b7826cd851dec80f86cc371
Imran <polboy777@gmail.com>
2019-10-17 14:06:34 +0600

    create thead list and thread list item components

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,23 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
+import ThreadShow from "@/components/ThreadShow";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
   routes: [
     {
       path: "/",
       name: "Hello",
       component: HelloWorld
+    },
+    {
+      path: "/thread/:id",
+      name: "ThreadShow",
+      component: ThreadShow,
+      props: true
     }
-  ]
+  ],
+  mode: "history"
 });
