1e1ca7f1d416fb4541f39e6ff4c3631c0cf945b3
pollmix <polboy777@gmail.com>
2019-10-20 12:10:53 +0600

    forum pages added

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,22 +2,25 @@
   <div class="col-full">
     <h1>Welcome to the Forum</h1>
     <ThreadList :threads="threads" />
+    <ForumList :forums="forums" />
   </div>
 </template>
 
 <script>
 import sourceData from "@/data";
 import ThreadList from "@/components/ThreadList";
-console.log(sourceData);
+import ForumList from "@/components/ForumList";
 
 export default {
   components: {
-    ThreadList
+    ThreadList,
+    ForumList
   },
 
   data() {
     return {
       threads: Object.values(sourceData.threads),
+      forums: Object.values(sourceData.forums),
       posts: sourceData.posts,
       users: sourceData.users
     };
