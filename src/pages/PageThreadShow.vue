4d0030d495c53de9162b2d3176869065ab787221
pollmix <polboy777@gmail.com>
2019-10-20 11:08:45 +0600

    PostEditor component added

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,16 +2,19 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts" />
+    <PostEditor @save="addPost" :threadId="id" />
   </div>
 </template>
 
 <script>
 import sourceData from "@/data";
 import PostList from "@/components/PostList";
+import PostEditor from "@/components/PostEditor";
 
 export default {
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
 
   props: {
@@ -23,8 +26,22 @@ export default {
 
   data() {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ""
     };
+  },
+
+  methods: {
+    addPost(eventData) {
+      const post = eventData.post;
+      const postId = eventData.post[".key"];
+
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+
+      this.newPostText = "";
+    }
   },
 
   computed: {
