81ad66f784d071b887174bb73091039d9c4dd30f
pollmix <polboy777@gmail.com>
2019-10-27 13:14:51 +0600

    fix newly created thread edit bug

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,8 @@ export default new Vuex.Store({
       commit("setPost", { post, postId });
       commit("appendPostToThread", { threadId: post.threadId, postId });
       commit("appendPostToUser", { userId: post.userId, postId });
+
+      return Promise.resolve(state.posts[postId]);
     },
 
     createThread({ state, commit, dispatch }, { text, title, forumId }) {
@@ -46,7 +48,12 @@ export default new Vuex.Store({
         commit("appendThreadToForum", { forumId, threadId });
         commit("appendThreadToUser", { userId, threadId });
 
-        dispatch("createPost", { text, threadId });
+        dispatch("createPost", { text, threadId }).then(post =>
+          commit("setThread", {
+            threadId,
+            thread: { ...thread, firstPostId: post[".key"] }
+          })
+        );
 
         resolve(state.threads[threadId]);
       });
